80510978896127cb53ad241ea058cde6d2743e50
astrolabe31 <renaud.gilbert@hotmail.fr>
2024-02-07 19:31:30 +0100

    update

--- git-save-master.sh
+++ git-save-master.sh
@@ -3,25 +3,15 @@ echo "####################### backup master ###################################"
 cd ./etcd/backup && ./backup.sh && tar czvf snapshot.db.tar.gz snapshot.db &&  rm snapshot.db
 echo "####################### COMPRESS data ###################################"
 echo "####################### PULL PUSH ###################################"
-# pwd="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"
-# echo "pwd : " ${pwd}
+
 git gc --aggressive --prune=now
-# git config credential.helper store
-# git config --global user.name "astrolabe31"
-# git remote set-url origin https://"astrolabe31":"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"@github.com/astrolabe31/master-node2.git
-# git config --global credential.helper '!f() { sleep 1; echo "username=astrolabe31 token=${pwd}"; }; f'
 git config --global push.default simple
 
 sleep 5
 git add --all
 git commit -m "update"  
 
-# git config --global http.sslVerify "false"
-
-# git clone https://renaud:glpat-918Lzrfc_phAFaBdewt7@gitlab.astrolabe31.fr/k8s/dashboard.git
-
-git pull 
-# https://github.com/astrolabe31/master-node2.git 
+git pull https://github.com/astrolabe31/master-node2.git main
 
 git push https://astrolabe31:xxx@github.com/astrolabe31/master-node2.git main
 
