e9a4f64762ace7fa919847900e596c6ffcf1a96f
root <root@laura.lan>
2024-02-07 18:00:25 +0100

    update

--- git-save-master.sh
+++ git-save-master.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 echo "####################### backup master ###################################"
 cd ./etcd/backup && ./backup.sh && tar czvf snapshot.db.tar.gz snapshot.db &&  rm snapshot.db
-# ./etcd/backup/backup.sh
 echo "####################### COMPRESS data ###################################"
-# tar czvf ./etcd/backup/snapshot.db.tar.gz ./etcd/backup/snapshot.db
-# rm ./etcd/backup/snapshot.db
 echo "####################### PULL PUSH ###################################"
-# git init
+
 echo "pwd : " $(pwd)
 git gc --aggressive --prune=now
 sleep 5
 git add --all
 git commit -m "update"  
-
 
 # git config --global http.sslVerify "false"
 
@@ -20,6 +16,7 @@ git commit -m "update"
 
 git pull https://github.com/astrolabe31/master-node2.git main
 
-git push https://github.com/astrolabe31/master-node2.git main
+git push https://astrolabe31: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
+@github.com/astrolabe31/master-node2.git main
 
 echo "####################### END ###################################"
