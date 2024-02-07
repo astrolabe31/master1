8ea6a7b85b088f8ec1b48f7b406d9382fc7328d3
root <root@laura.lan>
2024-02-07 16:52:15 +0100

    update

---
+++ git-save.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "####################### COMPRESS data ###################################"
+./etcd/backup/tar czvf snapshot.db.tar.gz snapshot.db
+rm ./etcd/backup/snapshot.db
+echo "####################### PULL PUSH ###################################"
+# git init
+echo "pwd : " $(pwd)
+sleep 5
+git add --all
+git commit -m "update"  
+
+
+# git config --global http.sslVerify "false"
+
+# git clone https://renaud:glpat-918Lzrfc_phAFaBdewt7@gitlab.astrolabe31.fr/k8s/dashboard.git
+
+git pull https://github.com/astrolabe31/master-node2.git main
+
+git push https://github.com/astrolabe31/master-node2.git main
+
+echo "####################### END ###################################"
