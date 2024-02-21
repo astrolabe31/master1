1ecbc4a32667f3c0c1cc89fdfabaa8a4f2042c65
astrolabe31 <renaud.gilbert@hotmail.fr>
2024-02-21 10:31:45 +0100

    update

--- etcd/backup/backup.sh
+++ etcd/backup/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "####################### PULL PUSH ###########################"
+echo "####################### backup ###########################"
 ETCDCTL_API=3 etcdctl snapshot save snapshot.db --cacert /etc/kubernetes/pki/etcd/ca.crt --cert /etc/kubernetes/pki/etcd/server.crt --key /etc/kubernetes/pki/etcd/server.key
